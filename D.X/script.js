fe3865030086404f4aa61923464211a3deec3180
hassanhamdanr <hamdannaoh@gmail.com>
2025-08-05 11:28:10 +0100

    noh

---
+++ D.X/script.js
@@ -0,0 +1,8 @@
+const bon = document.querySelector("button");
+const bdoy = document.querySelector("body");
+btn .addEvenListener("click",funtion(){
+    body.style.backgroundColor = "#"+Math.random
+    (100000,999999);
+    console.log"#"+Math.random(100000,999999);
+});
+
